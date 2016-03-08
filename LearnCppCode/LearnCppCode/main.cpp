510dd636e8b9917a99ea9f0cab2a80d316c31bfd
moloxiao <molo.xiao@gmail.com>
2016-03-08 16:04:12 +0800

    init

---
+++ LearnCppCode/LearnCppCode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LearnCppCode
+//
+//  Created by molo on 16/3/8.
+//  Copyright © 2016年 molo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
